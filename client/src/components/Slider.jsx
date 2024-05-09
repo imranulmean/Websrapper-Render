70bb2f1d2d0061681f9265628b4ec75535c6c313
Imranul Hasan <imranulhasan73@gmail.com>
2024-05-09 13:51:24 +0600

    image style update

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -13,7 +13,7 @@ export default function Slider() {
       <section style={{'background-size': 'cover'}} class="bg-center bg-size-cover bg-no-repeat bg-[url('https://www.shutterstock.com/image-photo/various-dairy-products-600nw-627224804.jpg')] bg-blend-multiply">
           <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
               <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0" style={{'padding':'90px'}}>
-              <Link to="/search" class="inline-flex justify-center items-center py-3 px-3 text-base font-medium text-center text-zinc-950 rounded-lg bg-zinc-950 bg-opacity-20 hover:bg-opacity-75 border border-gray-900 hover:border-zinc-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+              <Link to="/search" class="inline-flex justify-center items-center py-3 px-3 text-base font-medium text-center text-yellow-950 rounded-lg bg-zinc-950 bg-opacity-10 hover:bg-opacity-75 border border-yellow-900 hover:border-zinc-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                     View all products
                       <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
