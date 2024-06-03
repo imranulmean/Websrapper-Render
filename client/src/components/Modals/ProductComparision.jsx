da680f63777d9a38bd62c506f9f4ccc7b2acfebe
imranulmean <imranulhasan73@gmail.com>
2024-06-04 00:48:09 +0600

    Query Type added

--- client/src/components/Modals/ProductComparision.jsx
+++ client/src/components/Modals/ProductComparision.jsx
@@ -13,6 +13,7 @@ export default function ProductComparisionModal({ post, showModal,  setShowModal
     const initialRender = useRef(true);
     const [comparisonProducts, setComparisonProducts]=useState([]);
     const [weightsProducts, setWeightsProducts]=useState([]);
+    const [brandWeightProducts, setBrandWeightProducts]=useState([]);
     const [loading, setLoading]=useState(false);
     const searchTerm=post.productTitle.replace(' |','');
     const productPrice = post.productPrice.toString(); // Convert productPrice to string
@@ -28,29 +29,40 @@ export default function ProductComparisionModal({ post, showModal,  setShowModal
 
     const comparisonApi = async () =>{
         const urlParams = new URLSearchParams(location.search);
-        const searchQuery = new URLSearchParams({
+        let searchQueryObj={
             searchTermFromUrl:urlParams.get('searchTerm'),
             searchTerm: searchTerm,
             productPrice: productPrice,
             mainCategoryName:post.mainCategoryName
-        }).toString();          
+        }
+        
         setLoading(true);
-        const res = await fetch(`/api/products/getComparisonProducts_with_Type_Weights?${searchQuery}`);
+        searchQueryObj['queryType']="brand_category";
+        let searchQuery = new URLSearchParams(searchQueryObj).toString();
+        const fetch_brand_category = fetch(`/api/products/getComparisonProducts_with_Type_Weights?${searchQuery}`);
+
+        searchQueryObj['queryType']="category_weight";
+        searchQuery = new URLSearchParams(searchQueryObj).toString();
+        const fetch_category_weight = fetch(`/api/products/getComparisonProducts_with_Type_Weights?${searchQuery}`);
+
+        searchQueryObj['queryType']="brand_weight";
+        searchQuery = new URLSearchParams(searchQueryObj).toString();
+        const fetch_brand_weight = fetch(`/api/products/getComparisonProducts_with_Type_Weights?${searchQuery}`);        
+        
+        const [brand_category_res, category_weight_res, brand_weight_res]= await Promise.all([fetch_brand_category, fetch_category_weight, fetch_brand_weight]);
         // if(res.ok){
             setLoading(false);
-            const data= await res.json();
+            const data= await brand_category_res.json();
             setComparisonProducts(data.products);
         // }
-        const weightRes = await fetch(`/api/products/getComparisonProducts_with_Only_Weights?${searchQuery}`,{
-            method:"POST",
-            headers: {
-                "Content-Type": "application/json" // Set the Content-Type header
-            },            
-            body:JSON.stringify(data.products)
-        });        
-        if(weightRes.ok){
-            const weightData= await weightRes.json();
+
+        if(category_weight_res.ok){
+            const weightData= await category_weight_res.json();
             setWeightsProducts(weightData.products); 
+        }
+        if(brand_weight_res.ok){
+            const brandWeightData= await brand_weight_res.json();
+            setBrandWeightProducts(brandWeightData.products);             
         }
     } 
 
@@ -64,25 +76,9 @@ export default function ProductComparisionModal({ post, showModal,  setShowModal
       <Modal show={showModal} onClose={() => setShowModal(false)} popup size='8xl'>
       <Modal.Header></Modal.Header>
         <Modal.Body className="p-1">
-          <div className='grid grid-cols-3 gap-1'>                
+          <div className='grid grid-cols-3 gap-1 md:grid-cols-4 '>                
                 {/* Main Product Left*/}
-                <div className='col-span-1 inline-grid'>                 
-                 {/* <div>                    
-                    <h5 class="text-lg font-semibold tracking-tight text-gray-900 dark:text-white">Selected Product</h5>
-                    <div class="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <img onClick={()=>setShowModal(true)} class="p-1 rounded-t-lg" src={post.productImage} alt="product image" />
-                        <div class="px-2 pb-2">
-                            <h5 class="text-md font-semibold tracking-tight text-gray-900 dark:text-white">{post.productTitle}</h5>
-                            <div class="flex items-center justify-between">
-                                <span class="text-sm font-medium text-gray-600 dark:text-white">{post.shop}</span>
-                            </div>
-                            <div class="flex items-center justify-between">
-                                <span class="text-md font-bold text-gray-900 dark:text-white">${post.productPrice}</span>
-                            </div>                            
-                        </div>                        
-                    </div>
-                    <Button size="xs" color="dark" onClick={()=>addToCart(post)}>Add To Cart</Button>                                         
-                  </div>     */}
+                <div className='hidden md:col-span-1 md:inline-grid '>
                     <Table hoverable>
                         <Table.Head>
                             <Table.HeadCell>Selected Product</Table.HeadCell>
@@ -107,7 +103,7 @@ export default function ProductComparisionModal({ post, showModal,  setShowModal
                     </Table>                                
                 </div>
                 {/* Comparision Products Right */}
-                <div className='col-span-2 h-[650px] overflow-y-auto'>
+                <div className='col-span-3 h-[650px] overflow-y-auto'>
                      {/* /////////////////// */}
                      {
                         loading && 
@@ -119,7 +115,7 @@ export default function ProductComparisionModal({ post, showModal,  setShowModal
                         <div>
                             <Table hoverable>
                                 <Table.Head>
-                                    <Table.HeadCell>Same Brand Comparision Products</Table.HeadCell>
+                                    <Table.HeadCell>Same Brand Same Category</Table.HeadCell>
                                 </Table.Head>
                                 <Table.Body className="divide-y">
                                     {
@@ -139,7 +135,7 @@ export default function ProductComparisionModal({ post, showModal,  setShowModal
                         <div>
                             <Table hoverable>
                                 <Table.Head>
-                                    <Table.HeadCell>Same Weighted Products:</Table.HeadCell>
+                                    <Table.HeadCell>Same Category Same Weight:</Table.HeadCell>
                                 </Table.Head>
                                 <Table.Body className="divide-y">
                                     {
@@ -156,41 +152,27 @@ export default function ProductComparisionModal({ post, showModal,  setShowModal
                                 </Table.Body>
                             </Table>
                         </div>
+                        <div>
+                            <Table hoverable>
+                                <Table.Head>
+                                    <Table.HeadCell>Same Brand Same Weight:</Table.HeadCell>
+                                </Table.Head>
+                                <Table.Body className="divide-y">
+                                    {
+                                        brandWeightProducts.length<1 &&
+                                        <p className='text-md font-semibold tracking-tight text-zinc-950 dark:text-white'>No Compared Products Less than The Selected Price</p>
+                                    }
+                                    {
+                                        brandWeightProducts.map((product) =>
+                                            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                                <ProductCard product={product} addToCart={addToCart}/>
+                                            </Table.Row>                                                                 
+                                        )
+                                    }
+                                </Table.Body>
+                            </Table>
+                        </div>                        
                     </div>
-
-                    {/* //////////////////////// */}
-                    {/* <h5 class="text-lg font-semibold tracking-tight text-gray-900 dark:text-white">Same Brand Comparision Products</h5>
-                      {
-                        loading && 
-                        <div className='flex justify-center items-center'>
-                            <Spinner size='xl' />
-                        </div>                        
-                      }
-                    <div>
-                        <div className='grid grid-cols-2'>
-                            {
-                                comparisonProducts.length<1 &&
-                                <p className='text-md font-semibold tracking-tight text-zinc-950 dark:text-white'>No Compared Products Less than The Selected Price</p>
-                            }
-                            {
-                                comparisonProducts.map((product) =>
-                                <div className='col-span-1'>
-                                    <ProductCard product={product} addToCart={addToCart}/>
-                                </div>
-                                )
-                            }                      
-                        </div>                        
-                    </div>  
-                    <h5 class="text-lg font-semibold tracking-tight text-gray-900 dark:text-white">Same Weighted Products:</h5>
-                    <div className='grid grid-cols-2'>                        
-                        {
-                                weightsProducts.map((product) =>
-                                <div className='col-span-1'>
-                                    <ProductCard product={product} addToCart={addToCart}/>                               
-                                </div>
-                                )
-                            }                         
-                    </div> */}
                 </div>
           </div>
         </Modal.Body>
