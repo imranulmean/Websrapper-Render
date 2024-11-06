61d390c43c3efa3ae14fc8c455bed0f7298db6b3
Imranul Hasan <imranulhasan73@gmail.com>
2024-11-06 17:10:26 +0600

    promotion modal color changed

--- client/src/components/Modals/PromotionModal.jsx
+++ client/src/components/Modals/PromotionModal.jsx
@@ -26,7 +26,7 @@ export default function PromotionModal() {
                     <div  style={{"flexBasis":"50%"}}>
                         <img className='w-full' src="https://images.pexels.com/photos/1697902/pexels-photo-1697902.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
                     </div>
-                    <div className='flex flex-col justify-center'  style={{"flexBasis":"50%", 'background':'#FFBB00'}}>
+                    <div className='flex flex-col justify-center'  style={{"flexBasis":"50%", 'background':'linear-gradient(45deg, #0075bd, #FFBB00)'}}>
                         <h1 className='product-title' 
                         style={{'font-size':'30px',"font-weight":"600",'color':'#000'}}>Welcome To The Right Place</h1>
                     </div>
